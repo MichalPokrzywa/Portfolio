f7f11606d6fbecdcdeb56e846467814b4fd572e6
MichalPokrzywa <michpok467@student.polsl.pl>
2024-08-05 23:24:24 +0200

    quick fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Portfolio/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Portfolio/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/skills">Skills</Link></li>
