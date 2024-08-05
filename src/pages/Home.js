7095be8eb9d11cb9a835bb33ba65c53fe73befb1
MichalPokrzywa <michpok467@student.polsl.pl>
2024-08-05 23:35:21 +0200

    sdas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,12 @@ import Testimonials from '../components/Testimonials';
 import Contact from '../components/Contact';
 
 export default function Home() {
-  return (
-    <main>
-      <About />
-      <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact />
-    </main>
-  );
-}
+    return (
+      <main>
+        <section>
+          <h2>Welcome to My Portfolio</h2>
+          <p>This is the home page of my portfolio. You can navigate to different sections using the navbar above.</p>
+        </section>
+      </main>
+    );
+  }
