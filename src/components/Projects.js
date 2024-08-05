eb95336ecc87192368e945178f2e051bd1937432
MichalPokrzywa <michpok467@student.polsl.pl>
2024-08-05 22:56:36 +0200

    added pages

---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Projects() {
+    return (
+      <section>
+        <h2>Projects</h2>
+        <p>This is the Projects section.</p>
+      </section>
+    );
+  }
---
+++ src/components/Testimonials.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Testimonials() {
+  return (
+    <section>
+      <h2>Testimonials</h2>
+      <p>This is the Testimonials section.</p>
+    </section>
+  );
+}
