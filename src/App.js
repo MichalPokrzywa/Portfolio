817dfa0afe22c69a18ed046ee0093b1934b43b25
MichalPokrzywa <michpok467@student.polsl.pl>
2024-08-05 23:16:40 +0200

    dasdas

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/Portfolio/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
