6829f22d03d2b13d2fc81e585b874ed62d145be7
MichalPokrzywa <michpok467@student.polsl.pl>
2024-08-06 12:51:13 +0200

    git

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Profile from "./components/Profile";
 
 export default function App() {
   return (
-    <main >
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-    </main>
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
   );
 }
