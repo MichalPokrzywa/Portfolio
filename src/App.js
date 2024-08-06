4cdb0f8d1d8f3b52d845bcacb9366a4be875b880
MichalPokrzywa <michpok467@student.polsl.pl>
2024-08-06 12:52:21 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
 import Profile from "./components/Profile";
 
 export default function App() {
